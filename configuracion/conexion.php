65b2ec3ba78b1a67e88be9b1be3215a04936ac1f
ReymanBarra <rpapillo27@gmail.com>
2025-11-16 15:04:40 -0600

    Modify database connection and error handling
    Updated database connection details and error handling.

--- configuracion/conexion.php
+++ configuracion/conexion.php
@@ -4,14 +4,24 @@ class Conectar
 {
     private $conexion_bd;
 
-    // AHORA ES PUBLICA, NO PROTECTED
     public function conectar_bd()
     {
         try {
-            $this->conexion_bd = new PDO("mysql:host=localhost;dbname=verduleria", "root", "");
+            $this->conexion_bd = new PDO(
+                "mysql:host=185.27.134.11;dbname=icei_40334935_apiw_dbVerduleria;charset=utf8",
+                "icei_40334935",
+                "b8Gi91nFMkCh",
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                ]
+            );
+
             return $this->conexion_bd;
+
         } catch (Exception $e) {
-            print "Error en la base de datos: " . $e->getMessage();
+            echo "Error en la base de datos: " . $e->getMessage();
             die();
         }
     }
@@ -29,8 +39,9 @@ class Conectar
 function ejecutarConsulta($sql)
 {
     $db = new Conectar();
-    $pdo = $db->conectar_bd();            // AHORA SÍ SE PUEDE LLAMAR
+    $pdo = $db->conectar_bd();
     $db->establecer_codificacion();
+
     return $pdo->query($sql);
 }
 
@@ -41,7 +52,7 @@ function ejecutarConsultaSimpleFila($sql)
     $db->establecer_codificacion();
 
     $query = $pdo->query($sql);
-    return $query->fetch(PDO::FETCH_OBJ);
+    return $query->fetch();
 }
 
 function ejecutarConsulta_retornarID($sql)
