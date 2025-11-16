400c5872954e6143c90e0e2363d02c975097a098
ReymanBarra <rpapillo27@gmail.com>
2025-11-16 15:55:30 -0600

    Update conexion.php

--- configuracion/conexion.php
+++ configuracion/conexion.php
@@ -7,12 +7,22 @@ class Conectar
     public function conectar_bd()
     {
         try {
-            $this->conexion_bd = new PDO(
-                "mysql:host=mysql.railway.internal;port=3306;dbname=railway",
-                "root",
-                "xFDGacnilabXUBmAhpwRsfTUT0aYVAsi"
-            );
+
+            // Credenciales reales de Railway
+            $host = "shuttle.proxy.rlwy.net";   // HOST EXTERNO
+            $port = "46221";                    // PUERTO EXTERNO
+            $dbname = "railway";                // NOMBRE DE TU BD
+            $user = "root";                     // USUARIO
+            $pass = "xFDGacnilabXUBmAhpwRsfTUT0aYVAsi";  // CONTRASEÑA
+
+            $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+            $this->conexion_bd = new PDO($dsn, $user, $pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+
             return $this->conexion_bd;
+
         } catch (Exception $e) {
             print "Error en la base de datos: " . $e->getMessage();
             die();
