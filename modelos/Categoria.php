fd1b907bd2e96eee5087bfcdbf5d01bafff96bb5
stevenBalto <stevenbaltodano52@gmail.com>
2025-11-16 18:01:11 -0600

    integracion de cors

--- modelos/Categoria.php
+++ modelos/Categoria.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+// Importante: responder a OPTIONS para evitar el error CORS preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 // Clase Categoria hereda de la clase Conectar
 class Categoria extends Conectar {
 
