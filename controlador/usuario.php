562b447bca681382aa844a62edac816edb5f7990
stevenBalto <stevenbaltodano52@gmail.com>
2025-11-17 21:48:00 -0600

    otra prueba cors

--- controlador/usuario.php
+++ controlador/usuario.php
@@ -1,10 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Credentials: true");
 
-// Preflight request handling
+// Manejo de preflight
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
